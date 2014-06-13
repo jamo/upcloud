6c3ac359d8e22aa183ce69394de9bfea6f06a4d5
Jarmo Isotalo <jamo@isotalo.fi>
2014-06-14 01:12:36 +0300

    Stop function

---
+++ lib/upcloud/commands/stop.rb
@@ -0,0 +1,38 @@
+module Upcloud
+  module Command
+
+    require 'optparse'
+    class Stop < BaseCommand
+
+      def parameters
+        {uuid: @uuid}
+      end
+
+      def end_url
+
+        "/server/#{@uuid}/stop"
+      end
+
+      def do_it(args)
+        options = {}
+        OptionParser.new do |opts|
+          opts.banner = "Usage: upcloud stop [options]"
+          opts.on("-u", "--uuid uuid", "Servers UUID") do |v|
+            @uuid = v
+          end
+          opts.on_tail("-h", "--help", "Show this message") do
+            puts opts
+            exit
+          end
+        end.parse!(args)
+        p OPTIONS: options
+
+        puts "Starting #{@uuid}"
+        format post(end_url, parameters)
+      end
+
+    end
+
+  end
+end
+
